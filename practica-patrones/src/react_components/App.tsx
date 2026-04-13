8674d9c58cb3fb7125b9b355dbcc7c31c00bc93e
jaimeolucha <jaime.olucha@cuatroochenta.com>
2026-04-13 14:03:09 +0200

    feat: include the components of the panel

--- practica-patrones/src/react_components/App.tsx
+++ practica-patrones/src/react_components/App.tsx
@@ -1,10 +1,45 @@
+import { NotificationBanner } from "./NotificationBanner";
+import { OrderControlsPanel } from "./OrderControlsPanel";
+import { OrderSummaryPanel } from "./OrderSummaryPanel";
+import { useOrderForm } from "./logic/useOrderForm";
+
 function App() {
+    const orderForm = useOrderForm();
+    const {
+        state,
+        summaryOrder,
+        canConfirm,
+        canDownload,
+        message,
+        bannerMessage,
+        onValueChange,
+        onConfirmOrder,
+        onDownloadReport,
+        onResetOrder,
+    } = orderForm;
+
     return (
-        <>
-            <div>
-                <h1>Sistema de pedidos</h1>
-            </div>
-        </>
+        <main className="app-shell">
+            <section className="order-card">
+                <OrderControlsPanel
+                    values={state.values}
+                    onValueChange={onValueChange}
+                />
+
+                <OrderSummaryPanel
+                    orderId={state.orderId}
+                    summaryOrder={summaryOrder}
+                    canConfirm={canConfirm}
+                    canDownload={canDownload}
+                    message={message}
+                    onConfirm={onConfirmOrder}
+                    onDownloadReport={onDownloadReport}
+                    onNewOrder={onResetOrder}
+                />
+            </section>
+
+            <NotificationBanner message={bannerMessage} />
+        </main>
     );
 }
 
